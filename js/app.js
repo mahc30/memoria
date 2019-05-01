f0f60f5b7a66733f86a939eaed586e47acdbf40b
MHupb <miguel.hincapiec@upb.edu.co>
2019-05-01 18:25:06 -0500

    Modal Agregado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-var logos = [];
+/*var logos = [];
 logos.push("facebook.png");
 logos.push("google-logo.png");
 logos.push("stackOverflow.png");
@@ -22,11 +22,31 @@ window.onload = ()=>{
         logo_1.innerHTML = html;
         logo_2.innerHTML = html2;
         logo_3.innerHTML = html3;
-        /*logo_4.innerHTML = html;
+        logo_4.innerHTML = html;
         logo_5.innerHTML = html3;
         logo_6.innerHTML = html3;
         logo_7.innerHTML = html3;
         logo_8.innerHTML = html3;
         logo_9.innerHTML = html3;
-        */
+        
+} */
+
+var modal = document.getElementById('myModal');
+
+
+// When the user clicks on the button, open the modal
+function openModal(){
+	modal.style.display = "block";
+}
+
+function closeModal(){
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    closeModal()
+  }
 } 
+
